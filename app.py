623a8d5828038de2a82f4bc7a5bd7218b962d06e
AliTosJM <alitosjm@gmail.com>
2021-01-21 18:23:26 -0600

    Single verse

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ import random
 # Data Viz Pkgs
 import altair as alt
 import matplotlib
-
+from utils import *
 matplotlib.use("Agg")
+
+
+@st.cache
+def load_bible(data):
+    return pd.read_csv(data)
 
 
 def main():
@@ -20,12 +25,69 @@ def main():
     # stc.html(HTML_BANNER)
     menu = ["Home", "MultiVerse", "About"]
 
-    # df = load_bible("data/KJV_Bible.csv")
+    df = load_bible("data/KJV_Bible.csv")
 
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
         st.subheader("Single Verse Search")
+        # st.dataframe(df)
+
+        book_list = df['book'].unique().tolist()
+        book_name = st.sidebar.selectbox("Books", book_list)
+        chapter = st.sidebar.number_input("Chapter", 1)
+        verse = st.sidebar.number_input("Verse", 1)
+        bible_df = df[df['book'] == book_name]
+        # st.dataframe(bible_df)
+
+        # Layout
+        c1, c2 = st.beta_columns([2, 1])
+        # Single Verse Layout
+        with c1:
+            try:
+                selected_passage = bible_df[(bible_df["chapter"] == chapter)
+                                            & (bible_df["verse"] == verse)]
+                passage_details = "{0} Chapter::{1} Verse::{2}".format(book_name, chapter, verse)
+                st.info(passage_details)
+                passage = "{}".format(selected_passage["text"].values[0])
+                st.write(passage)
+            except Exception as e:
+                st.warning(e.args)
+            except:
+                st.warning("Book out of Range")
+
+        with c2:
+            st.success("Verse of the Day")
+            chapter_list = range(10)
+            verse_list = range(20)
+            ch_choice = random.choice(chapter_list)
+            vs_choice = random.choice(verse_list)
+            random_book_name = random.choice(book_list)
+
+            st.write("Book:{},Ch:{},Vs:{}".format(random_book_name, ch_choice, vs_choice))
+            rand_bible_df = df[df["book"] == random_book_name]
+
+            try:
+                randomly_selected_passage = rand_bible_df[
+                    (rand_bible_df["chapter"] == ch_choice)
+                    & (rand_bible_df["verse"] == vs_choice)
+                ]
+                mytext = randomly_selected_passage["text"].values[0]
+            except:
+                mytext = rand_bible_df[
+                    (rand_bible_df["chapter"] == 1) & (rand_bible_df["verse"] == 1)
+                ]["text"].values[0]
+
+            # st.write(mytext)
+
+            stc.html(HTML_RANDOM_TEMPLATE.format(mytext), height=300)
+
+        # Search Topic/Term
+        search_term = st.text_input("Term/Topic")
+        with st.beta_expander("View Results"):
+            retrieved_df = df[df["text"].str.contains(search_term)]
+            st.dataframe(retrieved_df[["book", "chapter", "verse", "text"]])
+
     if choice == "MultiVerse":
         st.subheader("MultiVerse Retrieval")
     else:
---
+++ utils.py
@@ -0,0 +1,14 @@
+
+HTML_RANDOM_TEMPLATE = """<div style='padding:10px;background-color:#E1E2E1;
+border-radius: 8px 34px 9px 26px;
+-moz-border-radius: 8px 34px 9px 26px;
+-webkit-border-radius: 8px 34px 9px 26px;
+border: 2px ridge #000000;'>
+<h5>Verse of the Day</h5>
+<p>{}</p></div>"""
+
+
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
+HTML_BANNER = """<div style="background-color:#464e5f;padding:10px;border-radius:10px">
+<h1 style="color:white;text-align:center;">StreamBible App </h1>
+</div>"""
